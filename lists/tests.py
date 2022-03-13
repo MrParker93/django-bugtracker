63d49c7bdf334e8601c65b9c204ad8a2933436e0
Marcus Parker <marcuseparker93@gmail.com>
2022-03-13 22:21:48 +0000

    Added lists model and one to one relationship with Items model.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -10,18 +10,19 @@ class HomePageTest(TestCase):
 
 
 class ListViewTest(TestCase):
-    def test_uses_list_template(self):
-        response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertTemplateUsed(response, "lists/list.html")
-
     def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
+        _list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=_list)
+        Item.objects.create(text="itemey 2", list=_list)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+
+    def test_uses__listtemplate(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "lists/list.html")
 
 
 class NewListTest(TestCase):
@@ -37,15 +38,23 @@ class NewListTest(TestCase):
         self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        _list = List()
+        _list.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = _list
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = _list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, _list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -53,4 +62,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, _list)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, _list)
